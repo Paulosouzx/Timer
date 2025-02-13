f8019b7f63d6f81f0388a4ceecc619fabe9cd552
Paulo Souza <paulosouzx14@gmail.com>
2025-02-13 19:52:44 +0000

    style: new logo add, new component Header and foulder style cointains color and global css

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -11,10 +11,14 @@ export const GlobalStyle = createGlobalStyle`
         outline: 0;
         box-shadow: 0, 0, 0, 2px ${(props) => props.theme['green-500']};
     }
+    /* html {
+        background-color: ${(props) => props.theme['gray-800']};
+    } */
 
     body{
         background-color: ${(props) => props.theme['gray-900']};
         color: ${(props) => props.theme['gray-300']};
+        -webkit-font-smoothing: antialiased;
     }
 
     body, input, textarea, button {
